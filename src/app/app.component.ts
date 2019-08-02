212d4962bb510ca224395f0fd4ad994aec074b41
Breno Silveira <brenodl@hotmail.com>
2019-08-01 21:36:40 -0300

    First Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  private _isNewEmailFormOpen = false;
+  emailList = [];
+  email = {
+    destinatario: '',
+    assunto: '',
+    conteudo: ''
+  }
+
+  handleNewEmail(event: Event){
+
+    event.preventDefault;
+
+    this.emailList.push(this.email);
+
+    this.email = {
+      destinatario: '',
+      assunto: '',
+      conteudo: ''
+    }
+  }
+
+  get isNewEmailFormOpen() {
+
+    return this._isNewEmailFormOpen;
+  }
+  toggleNewEmailForm() {
+
+    this._isNewEmailFormOpen = !this.isNewEmailFormOpen;
+  }
+}
